374896fe8db5413ce22cf33f3f8efae963eccc6c
Kantale Sujeeth <104816459+sujeethkantale04@users.noreply.github.com>
2024-06-24 18:40:27 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,79 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import env from "dotenv";
+
+const app = express();
+const port = 3000;
+env.config();
+
+const db = new pg.Client({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+});
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let items = [
+  { id: 1, title: "Buy milk" },
+  { id: 2, title: "Finish homework" },
+];
+
+app.get("/", async(req, res) => {
+    try{
+        const result = await db.query("SELECT * FROM items ORDER BY id ASC");
+        items = (result).rows;
+        res.render("index.ejs", {
+            listTitle: "Today",
+            listItems: items,
+        });
+    }catch(err){
+        console.log(err);
+    }
+});
+
+app.post("/add", async(req, res) => {
+    const item = req.body.newItem;
+    //   items.push({ title: item });
+    try{
+        await db.query(
+            "INSERT INTO items (title) VALUES ($1);",
+            [item]
+        ) 
+        res.redirect("/");
+    }catch(err){
+        console.log(err);
+    }
+});
+
+app.post("/edit", async (req, res) => {
+    const id = req.body.updatedItemId;
+    const item = req.body.updatedItemTitle;
+    try{
+        await db.query(
+            "UPDATE items SET title = $1 WHERE id = $2;",
+            [item,id]
+        )
+        res.redirect("/");
+    }catch(err){
+        console.log(err);
+    }
+});
+
+app.post("/delete", async(req, res) => {
+    const id = req.body.deleteItemId;
+    await db.query(
+        "DELETE FROM items WHERE id = $1;",
+        [id]
+    )
+    res.redirect("/");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
